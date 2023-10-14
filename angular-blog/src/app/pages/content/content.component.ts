d31072ed22dfdcb999f4057f8cff5986d07c4b45
valclemir <valclemir88@gmail.com>
2023-10-14 19:23:35 -0300

    adição de uma nova página

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  photocover:string="https://i.pinimg.com/originals/93/03/ed/9303ed216f918c12fb1769de1ceb30e5.jpg"
+  contenttitle:string="mundo"
+  description:string="ola"
   constructor() { }
 
   ngOnInit(): void {
