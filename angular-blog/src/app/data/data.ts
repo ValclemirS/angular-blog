daf20137e9860da4920d608a2bb0bf2ca3cc990c
valclemir <valclemir88@gmail.com>
2023-10-17 00:31:56 -0300

    update de informações

--- angular-blog/src/app/data/data.ts
+++ angular-blog/src/app/data/data.ts
@@ -1,19 +1,18 @@
 export const data = [
     {
     "id":'1',
-    "title":"luffy",
+    "title":"TI",
     "description":
-    "At the time of my knowledge cutoff date in September 2021, Gear 5 was not introduced in the One Piece manga or anime series. Luffy's transformation techniques, such as Gear Second and Gear Fourth, were significant power-ups that he used during battles.",
-    "photocover":"https://i.pinimg.com/1200x/48/51/d0/4851d00d687ab2582185de57908f8206.jpg"
+    "A evolução da TI é constante e rápida. A tendência atual inclui a expansão da computação em nuvem, a inteligência artificial, a Internet das Coisas (IoT) e a cibersegurança avançada. O futuro da TI promete soluções ainda mais sofisticadas e uma influência crescente em todos os aspectos da nossa vida cotidianaEm resumo a Tecnologia da Informação é um campo vasto e em constante evolução que desempenha um papel vital na era digital Entender o que é TI e seu impacto é essencial para se manter atualizado e para abraçar a oportunidades que ela oferece",
+    "photocover":"https://www.oneit.com.br/wp-content/uploads/2021/03/ti.jpg"
     },
     {
     "id":
     '2',
     "title":
-    "luffy novo top lane da loud"
-    ,
-     "description":"Após o fracasso no worlds 2023 loud manda demite o loud robo e contrata o muguiara para posição de top lane",
-     "photocover":"https://pbs.twimg.com/media/FM9WI4EX0AUgSed.jpg"
+    "infrestuta de rede",
+     "description":" Infraestrutura de tecnologia da informação (TI) refere-se aos componentes necessários para executar e gerenciar ambientes de TI empresarial. A infraestrutura de TI pode ser implantada em um sistema de cloud computing ou nas próprias instalações da organização.",
+     "photocover":"https://inputec.com.br/wp-content/uploads/2021/05/Infraestrutura-de-rede.jpg"
     }
 
 ]
